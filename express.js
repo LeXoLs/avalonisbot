804c5d6f5aceacedc3e47047e1bb29b972ee30c9
Glitch (avalon-bot-lol) <none>
2020-06-20 17:30:39 +0000

    ⏰🚣 Updated with Glitch

--- express.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const app = express()
-
-app.get('/', (req, res) => res.send('Ok'))
-
-app.listen(process.env.PORT, () => console.log("Avalon is listening on port " + process.env.PORT)) 
- 
-
-
